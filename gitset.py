6946e6f47dc7684252c464c789bbd79920f6676b
Kim lee hyun <lh7721004@gmail.com>
2021-11-27 18:59:40 +0900

    first commit

---
+++ gitset.py
@@ -0,0 +1,14 @@
+import os
+import time
+url = "https://github.com/lh7721004/auto-git-repository.git"
+os.system("git init")
+time.sleep(3)
+os.system("git add .")
+time.sleep(3)
+os.system("git commit -m \"first commit\"")
+time.sleep(3)
+os.system("git branch -M main")
+time.sleep(3)
+os.system("git remote add origin "+url)
+time.sleep(3)
+os.system("git push -u origin main")
