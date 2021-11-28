5fb137f4631bef8991e553518115e2299a5d2cfb
Kim lee hyun <lh7721004@gmail.com>
2021-11-28 10:30:43 +0900

    first commit

--- gitset.py
+++ gitset.py
@@ -1,5 +1,18 @@
 import os
 import time
+import pickle
+try:
+    with open("count.pickle","rb") as f:
+        count = pickle.load(f)
+except:
+    pass
+
+
+
+
+
+
+
 url = "https://github.com/lh7721004/auto-git-repository.git"
 path = ""
 os.system("cd "+path)
@@ -14,3 +27,6 @@ time.sleep(3)
 os.system("git remote add origin "+url)
 time.sleep(3)
 os.system("git push -u origin main")
+
+with open("count.pickle","wb") as f:
+    pickle.dump(count,f)
